d557543817407f3f328e9bfe29d18a6ab6f2d9e1
pokotsun <gorigolila@gmail.com>
2019-03-26 15:04:02 +0900

    template.cppのアップデート

--- .vim/templates/template.cpp
+++ .vim/templates/template.cpp
@@ -6,6 +6,16 @@ using namespace std;
 #define OUT(P) cout << (P) << endl
 #define OUT2(P1, P2) cout << (P1) << " " << (P2) << endl
 
+// integerの各桁を足す
+int sum_digits(int num) {
+    int rtn = 0;
+    string num_str = to_string(num);
+    for(int i=0; i<num_str.size(); i++) {
+        rtn += num_str[i] - '0';
+    } 
+    return rtn;
+}
+
 int main() {
 
     return 0;
